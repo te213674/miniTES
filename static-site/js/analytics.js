343e1a2565c0be6b49198e2800534b3f536cdf76
te213674 <te213674@gmail.com>
2026-04-18 19:24:58 +0300

    add new pages: services, rental, design, installation, maintenance + SEO files (robots.txt, sitemap.xml, 404.html, analytics)

---
+++ static-site/js/analytics.js
@@ -0,0 +1,24 @@
+// Яндекс.Метрика
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108660105', 'ym');
+
+ym(108660105, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: "dataLayer",
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce: true,
+    trackLinks: true
+});
+
+// Google Analytics 4 (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-1ML71WLBB3');
